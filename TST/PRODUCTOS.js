433ee265a99607cfbee0ae95336b69916043c34c
Santiago Vicentin <svicentin2@gmail.com>
2024-09-11 09:35:22 -0300

    He añadido los archivos de dos proyectos básicos

---
+++ TST/PRODUCTOS.js
@@ -0,0 +1,9 @@
+function redirigir1() {
+    // Cambiar la URL por la que deseas redirigir
+    window.location.href = "./PRODUCTOS_campanasisla.html";
+}
+
+function redirigir2() {
+    // Cambiar la URL por la que deseas redirigir
+    window.location.href = "./PRODUCTOS_campanaspared.html";
+}
